b6ff783a650a1ff765eb8bfddad56651652603cb
Gustavo Dorow <gustavosdorow@gmail.com>
2026-01-19 19:42:08 -0300

    HTTPS Streams: L4

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,15 +16,39 @@ func main() {
 	}
 	defer file.Close()
 
+	var current_line_contents string = ""
+
 	for {
 		buffer := make([]byte, 8)
 		n, err := file.Read(buffer)
 
-		if n > 0 {
-			fmt.Printf("read: %s\n", buffer[:n])
+		chunk := string(buffer[:n])
+		parts := strings.Split(chunk, "\n")
+
+		if err != nil {
+			if current_line_contents != "" {
+				fmt.Printf("read: %s\n", current_line_contents)
+				current_line_contents = ""
+			}
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
 		}
+		for i := 0; i < len(parts)-1; i++ {
+			frase := current_line_contents + parts[i]
+			fmt.Printf("read: %s", frase)
+			current_line_contents = ""
+		}
+		current_line_contents = current_line_contents + parts[len(parts)-1]
+
 		if err == io.EOF {
 			break
 		}
 	}
+	if current_line_contents != "" {
+		fmt.Printf("read: %s\n", current_line_contents)
+	}
+
 }
